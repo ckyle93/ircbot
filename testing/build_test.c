4fcc70347eb9e4617e1a7a316eebd68884a78f1c
Chris Kyle <ckyle@ucsc.edu>
2016-04-05 18:26:58 -0700

    Fix typo in read_lines

--- testing/build_test.c
+++ testing/build_test.c
@@ -5,11 +5,11 @@
 
 int main() {
     int i;
-    char *line_pos = "123456789012345678901234567890123456789012345678901234567890\n";
+    char *line_pos = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
 
     FILE* new_file = fopen("line_pos", "w");
-    for(i = 0; i < 100; ++i) {
-       fprintf(new_file, line_pos);
+    for(i = 1; i < 100; ++i) {
+       fprintf(new_file, "#%d %s\n", i, line_pos);
     }
     fclose(new_file);
 }
--- testing/parse_file.c
+++ testing/parse_file.c
@@ -1,7 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-
+#define BUFFER_SIZE 1024
+char buffer[BUFFER_SIZE];
+size_t inbuf_used = 0;
 
 // Based off of the example provided by bdonlan at
 // http://stackoverflow.com/questions/6090594/c-recv-read-until-newline-occurs
@@ -11,22 +24,22 @@ void read_lines(int fd) {
         fprintf(stderr, "Line exceeded buffer length. \n");
     }
 
-    ssize_t rv = read(fd, buffer, BUFFER_SIZE - 1);
+    ssize_t rv = read(fd, (void *)&buffer[inbuf_used], buf_remain);
     if (rv == 0) {
-        fprintf(stderr, "Connection closed.");
+        exit(EXIT_SUCCESS);
     }
     if (rv < 0 && errno == EAGAIN) {
         // Socket has no data for us
         return;
     }
     if (rv < 0) {
-       error("Connection error");
+       printf("Connection error");
     }
     inbuf_used += rv;
 
     char *line_start = buffer;
     char *line_end;
-    while( (line_end = memchr((void*)line_start, '\n', inbuf_used - (line_start -buffer)))){
+    while( (line_end = memchr((void*)line_start, '\n', inbuf_used - (line_start - buffer)))){
         *line_end = 0;
         printf("%s\n", line_start);
         line_start = line_end + 1;
@@ -36,5 +49,9 @@ void read_lines(int fd) {
 }
 
 int main() {
-    FILE *my_file = open("line_pos", O_READONLY);
-    
+    int fd = open("line_pos", O_RDONLY);
+    while(1) {
+        read_lines(fd);
+    }
+    close(fd);
+}
